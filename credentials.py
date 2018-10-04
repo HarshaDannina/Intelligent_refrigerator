48a96ff4e07bc3173c8bfe70e45b69a4d200a01e
Harsha Manoj <danninamanoj123@gmail.com>
2018-10-04 17:32:26 +0530

    Add files via upload

---
+++ credentials.py
@@ -0,0 +1,6 @@
+MAILGUN_API_KEY = 'your Mailgun private API'
+SANDBOX_URL = 'your sandbox url'
+SENDER_EMAIL = 'test@your sandbox url'
+RECIPIENT_EMAIL = 'your mail id'
+API_KEY = 'Bolt API key'
+DEVICE_ID = 'Your Device ID'
