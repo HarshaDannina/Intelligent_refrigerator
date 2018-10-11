c7b7ad9aa95705390a2ea3e35be618580e0a759d
Harsha Manoj <danninamanoj123@gmail.com>
2018-10-11 20:42:24 +0530

    Update dtree.py

--- dtree.py
+++ dtree.py
@@ -4,39 +4,43 @@ Created on Fri Oct  5 10:17:16 2018
 
 @author: HARSHA
 """
-
-import tensorflow as tf
+#importing libraries
 import pandas as pd
 import numpy as np
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import recall_score, precision_score
 
+#defining Machine Learning Classifier
 
 def MachineLearning_model(ldr_value):
 
+    #reading sensor data
     dataset = pd.read_csv("sensorvalues.csv")
     
+    #Converting door state from strings to binary labels.
     from sklearn.preprocessing import LabelBinarizer
     state_lb = LabelBinarizer()
     Y = state_lb.fit_transform(dataset.state.values)
     
-    #from sklearn.preprocessing import normalize
+    #Convert features into matrix form
     FEATURES = dataset.columns[0]
     X_data = dataset[FEATURES].as_matrix()
     X_data = X_data.reshape(-1,1)
     #X_data = normalize(X_data)
     
-    
+    #Splitting training and testing data
     from sklearn.model_selection import train_test_split
     X_train, X_test, y_train, y_test = train_test_split(X_data, Y, test_size=0.3, random_state=1)
     
     
-    
+    #Defining the Decision tree classifier
     
     clf = DecisionTreeClassifier()
     
-    
+    #Training the classifier
     clf.fit(X_train,y_train)
+    
+    #Testing the Classifier
     accuracy = clf.score(X_test, y_test)
     print(accuracy)
     pred = clf.predict(X_test)
@@ -46,12 +50,12 @@ def MachineLearning_model(ldr_value):
     print ("Recall:", recall)
     
     
-    #fig = plot_decision_regions(X_data, Y, clf=clf, legend=2, X_highlight=X_test)
-    #plt.show()
-    
-    
+    #Predicting the door state from LDR value
     state = clf.predict(ldr_value)
+    
+    #Returning door state in binary form to capstone-project.py
     return state
+
 #ldr_value = 900
 #state = MachineLearning_model(ldr_value)
 #print(state)
